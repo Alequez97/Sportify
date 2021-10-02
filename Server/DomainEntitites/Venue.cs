03e172f6fe358a4f97f2d0a97ad6cb04c5c97a0e
Jurijs Sluncevs <jurashluncev@gmail.com>
2021-10-02 15:05:49 +0300

    Class Venue has been added.

---
+++ Server/DomainEntitites/Venue.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DomainEntities
+{
+    class Venue
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Address { get; set; }
+    }
+}
