9df244c257150dfcfa3781ffacf8d21cf9a5dba9
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-10-02 15:46:35 +0300

    Add events controller and DataServices project

---
+++ Server/SportifyWebApi/Controllers/EventsController.cs
@@ -0,0 +1,17 @@
+﻿using DataServices;
+using DomainEntities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SportifyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        public EventsController()
+        {
+            
+        }
+
+    }
+}
