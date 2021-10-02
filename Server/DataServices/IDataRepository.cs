d7d925f0c2f00df9995b151731ef11adaebed775
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-10-02 15:53:17 +0300

    Add data repository interface

---
+++ Server/DataServices/IDataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServices
+{
+    public interface IDataRepository<T>
+    {
+
+        T Read(int id);
+        
+        List<T> ReadAll();
+    }
+}
---
+++ Server/DataServices/MsSqlDataRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataServices
+{
+    public class MsSqlDataRepository<T> : IDataRepository<T>
+    {
+
+        public MsSqlDataRepository(DbContext)
+        {
+
+        }
+        public T Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
