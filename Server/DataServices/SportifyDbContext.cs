e1b2c7a5b92661b571420e501ce8cdf8d6d22c3d
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-10-10 16:58:39 +0300

    SPORTIFY-1 Add authentication

--- Server/DataServices/SportifyDbContext.cs
+++ Server/DataServices/SportifyDbContext.cs
@@ -1,14 +1,15 @@
 ﻿using DomainEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataServices
 {
-    public class SportifyDbContext : DbContext
+    public class SportifyDbContext : IdentityDbContext<User>
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
-                @"Server=(localdb)\mssqllocaldb;Database=SportifyDb;Trusted_Connection=True");
+                @"Server=(localdb)\mssqllocaldb;Database=sportify_db;Trusted_Connection=True");
         }
         public DbSet<Event> Events { get; set; }
         public DbSet<Venue> Venues { get; set; }
