a88894fbe881ff2eeab41f35c1dbf2e588332bf8
Aleksandrs Vaguscenko <aleksandrs.vaguscenko@gmail.com>
2021-10-10 19:27:14 +0300

    SPORTIFY-7 Add local build script

---
+++ ConfigFiles/DeploymentScript/LocalDeployment.ps1
@@ -0,0 +1,69 @@
+param([Boolean]$Launch = $false)
+
+if ($Launch)
+{
+    Start-Process powershell.exe -Verb RunAs -ArgumentList ('-ExecutionPolicy Bypass -NoExit -Command ". {0}"' -f ($MyInvocation.MyCommand.Definition))
+    Exit
+}
+
+#Lauch PS in project's folder
+$Workspace = Split-Path -Path $MyInvocation.MyCommand.Path
+$Workspace = [System.IO.Path]::GetFullPath("$Workspace\..\..").TrimEnd("\\")
+Set-Location $Workspace
+
+$ErrorActionPreference = "Stop"
+$InformationPreference = "Continue"
+
+function Add-Migration
+{
+    param(
+        [String]$MigrationName = $(throw 'Migration name parameter is mandatory')
+    )
+
+    Install-Ef-Tool-If-Not-Exists
+    Set-Location $Workspace/Server/DataServices
+    dotnet ef migrations add $MigrationName
+    Set-Location ../..
+}
+
+function Undo-Migration
+{
+    Install-Ef-Tool-If-Not-Exists
+    Set-Location $Workspace/Server/DataServices
+    dotnet ef migrations remove
+    Set-Location ../..
+}
+
+function Database-Update
+{
+    Install-Ef-Tool-If-Not-Exists
+    Set-Location $Workspace/Server/DataServices
+    dotnet ef database update 
+    Set-Location ../..
+}
+
+function Install-Ef-Tool-If-Not-Exists
+{
+    $CheckResult = powershell.exe -nologo -noprofile -executionpolicy bypass -command "dotnet-ef"
+    if ($LASTEXITCODE -gt 0)
+    {
+        dotnet tool install --global dotnet-ef
+    }
+    else 
+    {
+        Write-Host "dotnet-ef tool already installed" -ForegroundColor Yellow 
+    }
+}
+
+function Write-Help
+{
+    Write-Output "*** Local Deployment avaliable commands ***"
+    Write-Output "am           (Add-Migration)      Creates migration in DataServices project"
+    Write-Output "um           (Undo-Migration)     Removes last migration"
+    Write-Output "db-update    (Database-Update)    Updates database from migration in DataServices project"
+}
+
+Set-Alias am Add-Migration
+Set-Alias um Undo-Migration
+Set-Alias db-update Database-Update
+Write-Help
---
+++ build.bat
@@ -0,0 +1,1 @@
+powershell.exe -ExecutionPolicy Bypass ".\ConfigFiles\DeploymentScript\LocalDeployment.ps1" -Launch $true
