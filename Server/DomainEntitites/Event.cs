a8a52535a651d4145bb7af7a4061431e7fb34760
Jurijs Sluncevs <jurashluncev@gmail.com>
2021-10-22 23:43:53 +0300

    Event now has BriefDesc and Category. Category class has been added.

--- Server/DomainEntitites/Event.cs
+++ Server/DomainEntitites/Event.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DomainEntities
@@ -10,14 +11,18 @@ namespace DomainEntities
     {
         public string Title { get; set; }
 
+        public Category Category { get; set; }
+
+        public string BriefDesc { get; set; }
+
         public string Description { get; set; }
 
         public Venue Venue { get; set; }
 
         public DateTime TimeOfTheEvent { get; set; }
 
-        //public User CreatorId { get; set; }
+        public User CreatorId { get; set; }
 
-        //public List<User> Contributors { get; set; }
+        public List<User> Contributors { get; set; }
     }
 }
