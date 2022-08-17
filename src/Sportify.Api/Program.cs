439d4bdcfd7d428f2dc6c6d938cf95fcbacbb6a4
JS <jurashluncev@gmail.com>
2022-08-17 12:26:08 +0300

    Got rid of Startup.cs.

--- src/Sportify.Api/Program.cs
+++ src/Sportify.Api/Program.cs
@@ -1,19 +1,95 @@
+using System.IO;
+using System.Text;
+using Sportify.DataServices;
+using Sportify.DomainEntities;
+//using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using SportifyWebApi.Endpoints.MappingProfiles;
+using Sportify.Api.Interfaces;
+using Sportify.Api.Services;
 
-namespace Sportify.Api;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
 {
-  public static void Main(string[] args)
-  {
-    CreateHostBuilder(args).Build().Run();
-  }
+  c.SwaggerDoc("v1", new OpenApiInfo { Title = "SportifyWebApi", Version = "v1" });
+  c.EnableAnnotations();
+});
 
-  public static IHostBuilder CreateHostBuilder(string[] args) =>
-      Host.CreateDefaultBuilder(args)
-          .ConfigureWebHostDefaults(webBuilder =>
-          {
-            webBuilder.UseStartup<Startup>();
-          });
+builder.Services.AddDbContext<SportifyDbContext>(options =>
+{
+  options.UseSqlServer(builder.Configuration.GetConnectionString("Local"));
+});
+
+builder.Services.AddTransient<IStorageService>(x => new FileSystemStorageService(Directory.GetCurrentDirectory() + "\\..\\..\\client\\static\\images\\sportsGroundImages", true));
+
+builder.Services.AddIdentity<User, IdentityRole<int>>()
+    .AddEntityFrameworkStores<SportifyDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.AddAuthentication(options =>
+{
+  options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+  options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+  options.SaveToken = true;
+  options.RequireHttpsMetadata = false;
+  options.TokenValidationParameters = new TokenValidationParameters()
+  {
+    ValidateIssuer = true,
+    ValidateAudience = true,
+    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+  };
+});
+
+//services.AddMediatR(typeof(List.Handler).Assembly);
+builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+builder.Services.AddCors(opt =>
+{
+  opt.AddPolicy("CorsPolicy", policy =>
+  {
+    policy.WithOrigins("http://localhost:3000", "http://192.168.0.125:3000", "http://192.168.8.166:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+  });
+});
+
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+  app.UseSwagger();
+  app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SportifyWebApi v1"));
 }
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+  endpoints.MapControllers();
+});
+
+app.Run();
--- src/Sportify.Api/Startup.cs
+++ src/Sportify.Api/Startup.cs
@@ -1,108 +1,108 @@
-using System.IO;
-using System.Text;
-using Sportify.DataServices;
-using Sportify.DomainEntities;
-//using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using SportifyWebApi.Endpoints.MappingProfiles;
-using Sportify.Api.Interfaces;
-using Sportify.Api.Services;
+//using System.IO;
+//using System.Text;
+//using Sportify.DataServices;
+//using Sportify.DomainEntities;
+////using MediatR;
+//using Microsoft.AspNetCore.Authentication.JwtBearer;
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using Microsoft.IdentityModel.Tokens;
+//using Microsoft.OpenApi.Models;
+//using SportifyWebApi.Endpoints.MappingProfiles;
+//using Sportify.Api.Interfaces;
+//using Sportify.Api.Services;
 
-namespace Sportify.Api;
+//namespace Sportify.Api;
 
-public class Startup
-{
-  public Startup(IConfiguration configuration)
-  {
-    Configuration = configuration;
-  }
+//public class Startup
+//{
+//  public Startup(IConfiguration configuration)
+//  {
+//    Configuration = configuration;
+//  }
 
-  public IConfiguration Configuration { get; }
+//  public IConfiguration Configuration { get; }
 
-  // This method gets called by the runtime. Use this method to add services to the container.
-  public void ConfigureServices(IServiceCollection services)
-  {
-    services.AddControllers();
-    services.AddSwaggerGen(c =>
-    {
-      c.SwaggerDoc("v1", new OpenApiInfo { Title = "SportifyWebApi", Version = "v1" });
-      c.EnableAnnotations();
-    });
+//  // This method gets called by the runtime. Use this method to add services to the container.
+//  public void ConfigureServices(IServiceCollection services)
+//  {
+//    services.AddControllers();
+//    services.AddSwaggerGen(c =>
+//    {
+//      c.SwaggerDoc("v1", new OpenApiInfo { Title = "SportifyWebApi", Version = "v1" });
+//      c.EnableAnnotations();
+//    });
 
-    services.AddDbContext<SportifyDbContext>(options =>
-    {
-      options.UseSqlServer(Configuration.GetConnectionString("Local"));
-    });
+//    services.AddDbContext<SportifyDbContext>(options =>
+//    {
+//      options.UseSqlServer(Configuration.GetConnectionString("Local"));
+//    });
 
-    services.AddTransient<IStorageService>(x => new FileSystemStorageService(Directory.GetCurrentDirectory() + "\\..\\..\\client\\static\\images\\sportsGroundImages", true));
+//    services.AddTransient<IStorageService>(x => new FileSystemStorageService(Directory.GetCurrentDirectory() + "\\..\\..\\client\\static\\images\\sportsGroundImages", true));
 
-    services.AddIdentity<User, IdentityRole<int>>()
-        .AddEntityFrameworkStores<SportifyDbContext>()
-        .AddDefaultTokenProviders();
+//    services.AddIdentity<User, IdentityRole<int>>()
+//        .AddEntityFrameworkStores<SportifyDbContext>()
+//        .AddDefaultTokenProviders();
 
-    services.AddAuthentication(options =>
-    {
-      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-      options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-    }).AddJwtBearer(options =>
-        {
-          options.SaveToken = true;
-          options.RequireHttpsMetadata = false;
-          options.TokenValidationParameters = new TokenValidationParameters()
-          {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidAudience = Configuration["JWT:ValidAudience"],
-            ValidIssuer = Configuration["JWT:ValidIssuer"],
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
-          };
-        });
+//    services.AddAuthentication(options =>
+//    {
+//      options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//      options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//      options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//    }).AddJwtBearer(options =>
+//        {
+//          options.SaveToken = true;
+//          options.RequireHttpsMetadata = false;
+//          options.TokenValidationParameters = new TokenValidationParameters()
+//          {
+//            ValidateIssuer = true,
+//            ValidateAudience = true,
+//            ValidAudience = Configuration["JWT:ValidAudience"],
+//            ValidIssuer = Configuration["JWT:ValidIssuer"],
+//            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+//          };
+//        });
 
-    //services.AddMediatR(typeof(List.Handler).Assembly);
-    services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+//    //services.AddMediatR(typeof(List.Handler).Assembly);
+//    services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 
-    services.AddCors(opt =>
-    {
-      opt.AddPolicy("CorsPolicy", policy =>
-            {
-          policy.WithOrigins("http://localhost:3000", "http://192.168.0.125:3000", "http://192.168.8.166:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
-        });
-    });
-  }
+//    services.AddCors(opt =>
+//    {
+//      opt.AddPolicy("CorsPolicy", policy =>
+//            {
+//          policy.WithOrigins("http://localhost:3000", "http://192.168.0.125:3000", "http://192.168.8.166:3000").AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+//        });
+//    });
+//  }
 
-  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-  {
-    if (env.IsDevelopment())
-    {
-      app.UseDeveloperExceptionPage();
-      app.UseSwagger();
-      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SportifyWebApi v1"));
-    }
+//  // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//  public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//  {
+//    if (env.IsDevelopment())
+//    {
+//      app.UseDeveloperExceptionPage();
+//      app.UseSwagger();
+//      app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SportifyWebApi v1"));
+//    }
 
-    app.UseHttpsRedirection();
+//    app.UseHttpsRedirection();
 
-    app.UseRouting();
+//    app.UseRouting();
 
-    app.UseCors("CorsPolicy");
+//    app.UseCors("CorsPolicy");
 
-    app.UseAuthentication();
+//    app.UseAuthentication();
 
-    app.UseAuthorization();
+//    app.UseAuthorization();
 
-    app.UseEndpoints(endpoints =>
-    {
-      endpoints.MapControllers();
-    });
-  }
-}
+//    app.UseEndpoints(endpoints =>
+//    {
+//      endpoints.MapControllers();
+//    });
+//  }
+//}
